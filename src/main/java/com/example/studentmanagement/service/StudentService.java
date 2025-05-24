15e76330aa02bc25f19d10baf4bc117210adef9d
abdullahRiyadh <abdullahriyadhcse@gmail.com>
2025-05-24 18:00:07 +0600

    Student service

---
+++ src/main/java/com/example/studentmanagement/service/StudentService.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagement.service;
+
+import com.example.studentmanagement.model.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    Student saveStudent(Student student);
+    List<Student> getAllStudents();
+    Student getStudentById(Long id);
+    Student updateStudent(Student student);
+    void deleteStudent(Long id);
+}
---
+++ src/main/java/com/example/studentmanagement/service/StudentServiceImpl.java
@@ -0,0 +1,60 @@
+package com.example.studentmanagement.service;
+
+
+import com.example.studentmanagement.model.Student;
+import com.example.studentmanagement.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).orElse(null);
+
+    }
+
+    @Override
+    public Student updateStudent(Long id, Student studentDetails)  {
+        Optional<Student> studentOptional = studentRepository.findById(id);
+        if (studentOptional.isPresent()) {
+            Student student = studentOptional.get();
+            student.setName(studentDetails.getName());
+            student.setEmail(studentDetails.getEmail());
+            return studentRepository.save(student);
+        }
+        else {
+        return null;
+    }
+
+    }
+
+    @Override
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+
+}
+
